fbd7ec823e670cde5d00dc9809f8b020c928949a
harshit <harsheetjain>
2021-04-03 19:49:18 +0800

    Authentication- Login, create account, forget password

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -1,33 +1,66 @@
 import {
-    LOGIN_REQUEST,
-    SUCCESS,
-    FAILURE,
-    LOGOUT_REQUEST
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    UPDATE_SUCCESS,
+    UPDATE_FAIL
 } from "../actions/types";
 
 const initialState = {
-    isLoggedIn: ""
+    jwt: localStorage.getItem('jwt'),
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
 };
 
 export default function (state = initialState, action) {
     switch (action.type) {
-        case LOGIN_REQUEST:
+        case USER_LOADING:
             return {
-                ...state
-            };
-        case SUCCESS:
+                ...state,
+                isLoading: true
+            }
+        case USER_LOADED:
             return {
-                isLoggedIn: action.payload
-            };
-        case FAILURE:
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            }
+        case LOGIN_SUCCESS:
+            localStorage.setItem('jwt', action.payload.jwt);
             return {
-                isLoggedIn: action.payload
-            };
-        case LOGOUT_REQUEST:
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                isLoading: false
+            }
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT_SUCCESS:
+        case REGISTER_SUCCESS:
+        case REGISTER_FAIL:
+            localStorage.removeItem('jwt');
             return {
-                ...state
-            };
+                ...state,
+                jwt: null,
+                user: null,
+                isAuthenticated: false,
+                isLoading: false
+            }
+        case UPDATE_SUCCESS:
+            return {
+                ...state,
+                user: action.payload
+            }
+        case UPDATE_FAIL:
+            return state
         default:
-            return state;
+            return state
     }
 }
