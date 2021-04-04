c8fd949fa7003d3b683cd3e65d474f08fde1e476
harshit <harsheetjain>
2021-04-04 22:45:59 +0800

    Alerts Configured

--- frontend/src/components/SingleVessel/SingleVessel.jsx
+++ frontend/src/components/SingleVessel/SingleVessel.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Alert, Button } from "react-bootstrap";
 import "./SingleVessel.css";
+import axios from "axios";
+import { connect } from "react-redux";
 
-export default function SingleVessel({match}) {
+function SingleVessel({ match, auth }) {
     const [item, setItem] = useState({
         abbrVslM: "",
         avg_SPEED: "",
@@ -26,14 +29,80 @@ export default function SingleVessel({match}) {
         voyage_CODE_INBOUND: "",
         vsl_VOY: ""
     });
+    const [errorMsg, setErrorMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
 
     useEffect(() => {
         fetchItems();
     }, [])
 
+    function tokenConfig() {
+        const token = auth.jwt;
+
+        //Headers
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        //If token, then add to headers
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+
+        return config;
+    }
+
+    function addAlert() {
+        const body = JSON.stringify({
+            "email": auth.user.username,
+            "fullVslM": item.fullVslM,
+            "inVoyN": item.inVoyN
+        });
+        axios.post("http://localhost:8080/saveAlertInDB/", body, tokenConfig())
+            .then(res => {
+                console.log(res.data);
+                setSuccessMsg("Alert successfully added.")
+                setErrorMsg("");
+            })
+            .catch(err => {
+                setErrorMsg("Error adding the alert.");
+                setSuccessMsg("");
+            })
+    }
+
+    function deleteAlert() {
+        const token = auth.jwt;
+        const headers = {
+            'Authorization': `Bearer ${token}`
+        }
+        const data = {
+            email: auth.user.username,
+            fullVslM: item.fullVslM,
+            inVoyN: item.inVoyN
+        }
+        axios.delete("http://localhost:8080/deleteAlert", { headers, data })
+            .then(res => {
+                setSuccessMsg("Alert successfully deleted.")
+                setErrorMsg("");
+            })
+            .catch(err => {
+                setErrorMsg("Error deleting the alert.");
+                setSuccessMsg("");
+            })
+    }
+
+
     const fetchItems = async () => {
-        fetch(`http://localhost:8080/vessels/${match.params.fullVslM}/${match.params.inVoyN}`)
-            .then(response => response.json())
+        const config = {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('jwt')}`
+            }
+        };
+
+        axios.get(`http://localhost:8080/vessels/${match.params.fullVslM}/${match.params.inVoyN}`, config)
+            .then(response => response.data)
             .then(items => {
                 setItem(items)
             })
@@ -49,14 +118,38 @@ export default function SingleVessel({match}) {
                     <p><span className="category">Average Speed:</span> {item.avg_SPEED == null ? "N/A" : item.avg_SPEED}</p>
                     <p><span className="category">Maximum Speed:</span> {item.max_SPEED == null ? "N/A" : item.max_SPEED}</p>
                     <p><span className="category">Berth Number:</span> {item.berthN == null ? "N/A" : item.berthN}</p>
-                    <p><span className="category">Distance To Go:</span> {item.distance_TO_GO  == null ? "N/A" : item.distance_TO_GO} </p>
+                    <p><span className="category">Distance To Go:</span> {item.distance_TO_GO == null ? "N/A" : item.distance_TO_GO} </p>
                     {item.is_PATCHING_ACTIVATED == 0 ? <p><span className="category">Berthing Time: </span> {item.predicted_BTR == null ? "N/A" : item.predicted_BTR} </p> : <p><span className="category">Berthing Time</span> {item.patching_PREDICTED_BTR == null ? "N/A" : item.patching_PREDICTED_BTR} </p>}
                     <p><span className="category">Unberthing Time:</span> {item.unbthgDt == null ? "N/A" : item.unbthgDt} </p>
                     <p><span className="category">Status:</span> {item.status == null ? "N/A" : item.status} </p>
+
                 </div>
                 <div class="col-6">
+                    <Button variant="success" onClick={addAlert}>
+                        Add Alert
+        </Button>
+                    <br /> <br />
+                    <Button variant="danger" onClick={deleteAlert}>
+                        Delete Alert
+        </Button>
+                    <br /> <br />
+                    {errorMsg && <Alert variant="danger">{errorMsg}</Alert>}
+                    {successMsg && <Alert variant="success">{successMsg}</Alert>}
                 </div>
             </div>
         </div>
+
     </div>
 }
+
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleVessel);
